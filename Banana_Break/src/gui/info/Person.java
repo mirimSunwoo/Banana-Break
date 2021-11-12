d3e2cf363d4fdcf1ab7fd7ec3e20ca686aa46755
Leesunwoo <73941301+mirimSunwoo@users.noreply.github.com>
2021-11-12 16:05:49 +0900

    시작화면 만들기, 회원가입 화면 구성(id,pw,name,age)

---
+++ Banana_Break/src/gui/info/Person.java
@@ -0,0 +1,72 @@
+package gui.info;
+
+public class Person {
+	// ë©¤ë²„ë³??ˆ˜
+	String ID,name,PW,gender;
+	int age;
+	
+	public Person() {
+		
+	}
+	// ?ƒ?„±? ?•¨?ˆ˜
+	public Person(String ID, String name, String PW, String gender, int age){
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
+		this.ID = ID;
+		this.PW = PW;
+	
+	}
+
+	// setter
+	
+
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+	public void setPW(String PW) {
+		this.PW = PW;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+	// getter
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getID() {
+		return ID;
+	}
+	public String getPW() {
+		return PW;
+	}
+	
+	
+	
+	
+		//toString()
+		public String toString() {
+			return name + "\t" + gender + "\t" + age + "\t" + ID + "\t" + PW + "\n";
+		}
+
+
+}
---
+++ Banana_Break/src/main_activity.java
@@ -0,0 +1,26 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.*;
+public class main_activity extends JFrame{
+	private Image background=new ImageIcon(main_activity.class.getResource("./images/mainFrame.png")).getImage();
+	
+	public main_activity() {
+		homeframe();
+	}
+	public void homeframe() {
+		setTitle("Banana Break");//타이틀
+		setVisible(true);	//창을 눈에 보이도록 함
+		setSize(721, 540);	//사이즈는 640*480
+		setResizable(false);	//사이즈 재조정 불가능
+		setLocationRelativeTo(null);	//창이 가운데에 뜨도록 함
+		setDefaultCloseOperation(EXIT_ON_CLOSE);	//창을 끄면 프로그램을 종료
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
+	}
+	public void paint(Graphics g) {//그리는 함수
+		g.drawImage(background, 0, 0, null);
+	}
+	public static void main(String[] args) {
+		new main_activity();
+	}
+}
